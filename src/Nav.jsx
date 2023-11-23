5480722c6b401931064554ee8732a496cf9c43b3
Ebuka <ebuka2264@yahoo.com>
2023-11-24 00:21:36 +0100

    feat:connected wallets succesfully

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,10 +1,45 @@
+import { useState } from 'react'
+import {Xumm} from 'xumm'
 
-const Nav = () => {
-    return (
-        <div>
+const xumm = new Xumm('5cb15a71-6272-4732-a143-f3e0153c286f') // Some API Key
 
-        </div>
-    )
-};
+function App() {
+  const [account, setAccount] = useState('')
 
-export default Nav;
+  const [appName, setAppName] = useState('')
+
+  xumm.user.account.then(a => setAccount(a ?? ''))
+  xumm.environment.jwt?.then(j => setAppName(j?.app_name ?? ''))
+
+  const logout = () => {
+    xumm.logout()
+    setAccount('')
+  }
+
+  
+
+  return (
+    <div className="App">
+      <h2>{ appName }</h2>
+      <div>
+        Hi <b>{ account }</b>
+      </div>
+      {
+        account === '' && !xumm.runtime.xapp
+          ? <button onClick={xumm.authorize}>Sign in</button>
+          : ''
+      }
+      {
+        account !== ''
+          ? <>
+              &nbsp;- or -&nbsp;
+              <button onClick={logout}>Sign Out</button>
+            </>
+          : ''
+      }
+      
+    </div>
+  )
+}
+
+export default App
