1f5099d02bc8cb2eac5412aeb7f09ad610bf8150
Ebuka <ebuka2264@yahoo.com>
2023-11-24 12:59:33 +0100

    feat: remeber state with local storage

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,32 +1,68 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {Xumm} from 'xumm'
 
 const xumm = new Xumm('5cb15a71-6272-4732-a143-f3e0153c286f') // Some API Key
 
 function App() {
   const [account, setAccount] = useState('')
-
+  //const [connected, setConnected] = useState(false)
   const [appName, setAppName] = useState('')
 
-  xumm.user.account.then(a => setAccount(a ?? ''))
+  useEffect(()=>{
+    async function output() {
+      if (window.localStorage.getItem("xumm_address").length > 0) {
+        const _account = localStorage.getItem('xumm_address');
+        setAccount(_account)
+        console.log("account on first render is ", account)
+      }
+    } output()
+  },[])
+
+  xumm.user.account.then(a => {
+    setAccount(a ?? '')
+    console.log("about to set account")
+    localStorage.setItem('xumm_address', account);
+  })
   xumm.environment.jwt?.then(j => setAppName(j?.app_name ?? ''))
 
+
   const logout = () => {
+    localStorage.removeItem("xumm_address")
     xumm.logout()
     setAccount('')
+    
   }
 
+  async function get_nfts() {
+    
+  }
+
+  const login = async () => {
+    if (window.localStorage.getItem("xumm_address") !== null) {
+      const _account = localStorage.getItem('xumm_address');
+      console.log(_account)
+      if (_account.length == 0) {
+        xumm.authorize()
+      }
+      setAccount(_account)
+      //console.log("clicked")
+    } else {
+      await xumm.authorize()
+    }
+  }
+
+  
   
 
   return (
     <div className="App">
       <h2>{ appName }</h2>
       <div>
-        Hi <b>{ account }</b>
+        Hi <b>{ account || "" }</b>
       </div>
       {
         account === '' && !xumm.runtime.xapp
-          ? <button onClick={xumm.authorize}>Sign in</button>
+          ? <button onClick={login}>Sign in</button>
           : ''
       }
       {
