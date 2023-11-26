c4f965395d7400abe0996e29f8c7d8b588b47d1a
Ebuka <ebuka2264@yahoo.com>
2023-11-26 12:35:34 +0100

    feat: fetch current iou tokens🔨

--- client/src/Nav.jsx
+++ client/src/Nav.jsx
@@ -8,9 +8,10 @@ const xumm = new Xumm('5cb15a71-6272-4732-a143-f3e0153c286f') // Some API Key
 function App() {
   const [account, setAccount] = useState('')
   const [nfts, setNfts] = useState([]);
-  //const [appName, setAppName] = useState('')
+  const [price, setPrice] = useState('')
   const [balance, setBalance] = useState('')
   const [tokenData, setTokenData] = useState([])
+  const[tokenAddress, setTokenAddress] = useState('')
 
 
   useEffect(()=>{
@@ -73,6 +74,12 @@ function App() {
     return str;
   }
 
+  async function getPrice() {
+    const { data: { data } } = await server.get(`price/${tokenAddress}`)
+    setPrice(data)
+    
+  }
+  
 
 //https://ipfs.io/ipfs/bafyreihcxsrh2nqzdrqzy66smi7u2upjy3ya6ib73nr4gs7xf3v2eipbbq/metadata.json
 
@@ -120,9 +127,15 @@ function App() {
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"/>
             </svg>
         </div>
-        <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Token..." required/>
+            <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Token..." required
+            value={tokenAddress} onChange={(e) => setTokenAddress(e.target.value)}
+
+            />
     </div>
-    <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <button type="submit" onClick={(event) => {
+            getPrice()
+            event.preventDefault()
+          }} className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
         </svg>
@@ -130,6 +143,22 @@ function App() {
     </button>
 </form>
 
+    {/**--------------------------------------start button----------------------------------------------------------------------- */}
+ 
+        
+
+{
+          price === '' 
+            ? <></>
+            :
+          <div className=''>
+            Token price is <b>{price} xrp</b><br/>
+          </div>
+        }
+
+
+
+ {/**--------------------------------------end Token price----------------------------------------------------------------------- */}
 
 
         {
